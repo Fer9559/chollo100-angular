fc46869b6663a659bb41e5a35e8b8fdd70c1b68b
Fernando Saez <fernandosaez95@hotmail.com>
2024-10-31 12:12:01 +0100

    listado chollos y subscripciones + footer v3.0

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 import { HomeServiceService } from '../../services/homeService.service';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, switchMap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { debounceTime, filter, switchMap, takeUntil } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -19,20 +19,22 @@ export class HomeComponent implements OnInit  {
 
   chollos: any[] = [];
   searchChollos = new FormControl(''); // FormControl para manejar la entrada de búsqueda
-  private subscription: Subscription | null = null; // Variable para almacenar la suscripción
+
+  // Subject para controlar la vida de la suscripción
+  private destroy$ = new Subject<void>();
 
   ngOnInit(): void {
     this.getAllChollos(); // Cargar todos los chollos al iniciar
 
-    // Almacenar la suscripción en la variable searchSubscription
-    this.subscription = this.searchChollos.valueChanges.pipe(
+     this.searchChollos.valueChanges.pipe(
       filter((titulo): titulo is string => titulo !== null),
       debounceTime(100),
       filter((titulo: string) => titulo.length >= 3),
       switchMap(titulo => {
         console.log('switchMap ejecutado con título:', titulo); // Log para observar cada emisión en switchMap
         return this.homeService.getCholloByTitle(titulo);
-      })
+      }),
+      takeUntil(this.destroy$) // Se completa cuando el componente se destruye
     ).subscribe({
       next: (chollos) => {
         console.log('Suscripción ejecutada con resultados:', chollos); // Log para observar cada emisión del observable
@@ -43,14 +45,12 @@ export class HomeComponent implements OnInit  {
       }
     });
 
-    console.log('Suscripción activa:', this.subscription); // Log para observar el estado de la suscripción
+    //console.log('Suscripción activa:', this.subscription); // Log para observar el estado de la suscripción
   }
 
-  // Cancelar la suscripción en ngOnDestroy
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.destroy$.next(); // Emitir valor para completar las suscripciones
+    this.destroy$.complete(); // Completar el Subject
   }
 
   // Método para limpiar la búsqueda y mostrar todos los chollos
