d9c975df9eec626d33ef9fa2fc5f267a9fafea61
Fernando Saez <fernandosaez95@hotmail.com>
2024-10-31 09:35:20 +0100

    listado chollos y subscripciones + footer

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,59 +1,71 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 import { HomeServiceService } from '../../services/homeService.service';
-import { AuthStatus } from '../../../auth/interfaces/auth-status.enum';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, switchMap } from 'rxjs';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit  {
 
   private authService = inject(AuthService);
-  public user = computed(() => this.authService.currentUser());
-
   private homeService = inject(HomeServiceService);
   private router = inject(Router);
 
-  chollos: any[] = []; // Array para almacenar los chollos
+  chollos: any[] = [];
   searchChollos = new FormControl(''); // FormControl para manejar la entrada de búsqueda
+  private subscription: Subscription | null = null; // Variable para almacenar la suscripción
 
   ngOnInit(): void {
     this.getAllChollos(); // Cargar todos los chollos al iniciar
 
-     // Suscripción a los cambios en el FormControl searchChollos
-    this.searchChollos.valueChanges.pipe(
+    // Almacenar la suscripción en la variable searchSubscription
+    this.subscription = this.searchChollos.valueChanges.pipe(
       filter((titulo): titulo is string => titulo !== null),
       debounceTime(100),
       filter((titulo: string) => titulo.length >= 3),
-      switchMap(titulo => this.homeService.getCholloByTitle(titulo))
+      switchMap(titulo => {
+        console.log('switchMap ejecutado con título:', titulo); // Log para observar cada emisión en switchMap
+        return this.homeService.getCholloByTitle(titulo);
+      })
     ).subscribe({
       next: (chollos) => {
-        this.chollos = chollos; // Muestra solo el chollo encontrado
+        console.log('Suscripción ejecutada con resultados:', chollos); // Log para observar cada emisión del observable
+        this.chollos = chollos;
       },
       error: (error) => {
         console.error('Error al buscar el chollo:', error);
       }
     });
+
+    console.log('Suscripción activa:', this.subscription); // Log para observar el estado de la suscripción
   }
 
-  cleanSearch(){
-    this.searchChollos.setValue(''); // Vaciar el input de búsqueda
+  // Cancelar la suscripción en ngOnDestroy
+  /*ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }*/
+
+  // Método para limpiar la búsqueda y mostrar todos los chollos
+  cleanSearch() {
+    this.searchChollos.setValue('');
     this.getAllChollos();
   }
 
-  // Método para obtener todos los chollos
   getAllChollos(): void {
     this.homeService.getAllChollos().subscribe({
       next: (data) => {
         this.chollos = data.sort((a: any, b: any) => {
           const dateA = new Date(a.updatedAt || a.createdAt).getTime();
           const dateB = new Date(b.updatedAt || b.createdAt).getTime();
-          return dateB - dateA; // Orden descendente (más reciente primero)
+          return dateB - dateA;
         });
       },
       error: (error) => {
@@ -64,53 +76,4 @@ export class HomeComponent {
 
 
 
-  // Método para buscar un chollo por título
-  /*searchChollo(): void {
-    console.log('metodo searchChollo.');
-    if (this.searchTitle.trim() === '') {
-      console.log('titulo vacio');
-      this.getAllChollos();
-
-    }
-
-      this.homeService.getCholloByTitle(this.searchTitle).subscribe({
-        next: (chollos) => {
-          this.chollos = chollos; // Muestra solo el chollo encontrado
-        },
-        error: (error) => {
-          console.error('Error al buscar el chollo:', error);
-          this.chollos = []; // Si hay un error o no se encuentra nada, limpia el array
-        }
-      });
-
-  }*/
-
-
-
-
-
-  // Navega a la página de autenticación
-  login() {
-    this.router.navigate(['./auth']);
-  }
-
-/*
-  // Computed property para verificar si el usuario está autenticado
-  public isAuthenticated = computed<boolean>(() => {
-    return this.authService.authStatus() === AuthStatus.authenticated;
-  });
-
-  // Método para manejar la navegación según el estado de autenticación
-  navigateBasedOnAuthStatus() {
-    if (this.isAuthenticated()) {
-      this.router.navigateByUrl('/dashboard');
-    } else {
-      this.router.navigateByUrl('/auth/login');
-    }
-  }*/
-
-  // Cierra la sesión
-  onLogout() {
-    this.authService.logout();
-  }
 }
